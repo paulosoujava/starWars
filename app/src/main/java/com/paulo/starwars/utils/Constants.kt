cf99ff5ee3c2ef80effe40842d444a5d80488af5
paulosoujava <paulosoujava@gmail.com>
2023-01-12 08:17:06 -0300

    others [Constants] create paste and add constants

---
+++ app/src/main/java/com/paulo/starwars/utils/Constants.kt
@@ -0,0 +1,13 @@
+package com.paulo.starwars.utils
+
+class Constants {
+    companion object {
+        val URL_BASE_TO_IMAGE = "https://starwars-visualguide.com/assets/img/"
+        val CHARACTERS ="characteres"
+        val PLANETS ="planets"
+        val FILMS ="films"
+        val SPECIES ="species"
+        val VEHICLES ="vehicles"
+        val STARSHIPS ="starships"
+    }
+}
